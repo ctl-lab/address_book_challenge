28f6b690e9c407abb7268e9217cfa0486d7a87b9
emiliano-ma <emilianomainero@gmail.com>
2020-08-14 09:10:02 +0200

    adds comments of project status with the intention push to a new feature branch

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -3,3 +3,15 @@ describe('user can create a contact', () => {
         cy.visit('http://localhost:3001')
     })
 })
+
+// lo ultimo que hice fue hacer que mi 
+// default branch sea master y master está
+// igual que project_setup
+// tendria que crear una branch para esta
+// feature, hacer el proximo paso en el 
+// proyecto y pushear a esta nueva rama
+// luego probar a hacer un pull request
+// hacia master y ver si me queda todo bien
+// y asi seguir trabajando en la feature branch
+// para hacer pull request to master al final
+// tal como hicimos en el proy. anterior
